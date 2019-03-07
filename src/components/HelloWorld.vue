61886d8ac93516b68e3b493843dd893429b01d79
cqm1994617 <1025760508@qq.com>
2019-03-07 10:11:48 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,137 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+}
+.box {
+  width: 800px;
+  margin: 50px;
+}
+.title {
+  color: #666;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+.remark {
+  margin-top: 20px;
+  margin-left: 40px;
+  color: #999;
+}
+</style>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div
+      class="box"
+      v-for="item in data"
+    >
+      <h3 class="title">{{item.title}}</h3>
+      <Steps :current="item.current">
+        <Step 
+          v-for="step in item.step" 
+          :title="step.title">
+        >
+        </Step>
+      </Steps>
+      <div class="remark">{{item.remark}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      data: [
+        {
+          title: "招商撮合平台",
+          step: [{
+            title: '产品设计阶段',
+            content: ''
+          }, {
+            title: 'demo阶段',
+            content: ''
+          }, {
+            title: '开发阶段',
+            content: ''
+          }, {
+            title: '测试阶段',
+            content: ''
+          }, {
+            title: '发布',
+            content: ''
+          }],
+          remark: '招商撮合还处于设计阶段，现在已有产品设计初稿，设计稿优化阶段持续时间约1-2周，下一阶段为产品demo阶段。',
+          current: 0
+        },
+        {
+          title: "数据印象",
+          step: [{
+            title: '产品设计阶段',
+            content: ''
+          }, {
+            title: 'demo阶段',
+            content: ''
+          }, {
+            title: '开发阶段',
+            content: ''
+          }, {
+            title: '测试阶段',
+            content: ''
+          }, {
+            title: '发布',
+            content: ''
+          }],
+          remark: '数据印象还处于设计阶段，现在已有产品设计初稿，设计稿优化阶段持续时间约1-2周，下一阶段为产品demo阶段。',
+          current: 0
+        },
+        {
+          title: "大数据臻选",
+          step: [{
+            title: '产品设计阶段',
+            content: ''
+          }, {
+            title: 'demo阶段',
+            content: ''
+          }, {
+            title: '开发阶段',
+            content: ''
+          }, {
+            title: '测试阶段',
+            content: ''
+          }, {
+            title: '发布',
+            content: ''
+          }],
+          remark: '大数据臻选还处于设计阶段，现在已有产品设计初稿，设计稿优化阶段持续时间约1-2周，下一阶段为产品demo阶段。',
+          current: 0
+        },
+        {
+          title: "智能客服",
+          step: [{
+            title: '产品设计阶段',
+            content: ''
+          }, {
+            title: 'demo阶段',
+            content: ''
+          }, {
+            title: '开发阶段',
+            content: ''
+          }, {
+            title: '测试阶段',
+            content: ''
+          }, {
+            title: '发布',
+            content: ''
+          }],
+          remark: '智能客服还处于设计阶段，现在已有产品设计初稿，设计稿优化阶段持续时间约1-2周，下一阶段为产品demo阶段。',
+          current: 0
+        }
+      ]
+    };
+  },
   props: {
     msg: String
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
